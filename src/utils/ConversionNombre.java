4839693b462aeedcd99e4520f45f03e2d6711205
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-03 19:24:21 +0100

    TP4 : Méthodes static

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		// TP 4
+
+		String nb = "125";
+		int newnb = Integer.parseInt(nb);
+		System.out.println(newnb);
+
+		int a = 12;
+		int b = 13;
+
+		System.out.println("Max(" + a + "," + b + ") = " + Integer.max(a, b));
+
+	}
+
+}
