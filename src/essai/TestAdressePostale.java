ce4e88ad55f780f97ff6df1106bbaf491f84f315
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-03 12:13:16 +0100

    tp1

--- src/essai/TestAdressePostale.java
+++ src/essai/TestAdressePostale.java
@@ -1,10 +1,15 @@
 package essai;
 
+import entites.AdressePostale;
+
 public class TestAdressePostale {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
-
+	AdressePostale adresse1 = new AdressePostale();
+	ap1.rue = 5;
+	AdressePostale adresse2 = new AdressePostale();
 }
