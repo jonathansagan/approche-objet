d35d0b1d7ec70325f414d5dc81ea3c37ce80c57c
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-03 19:23:52 +0100

    TP 3 : Instances et méthodes

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -1,17 +1,76 @@
 package entites;
 
 public class Personne {
-	
+
 	public String nom;
 	public String prenom;
 	public AdressePostale adresse;
 
-		
-	//2eme constructeur
-		
+	// 2eme constructeur
+
+	/**
+	 * Constructeur
+	 * 
+	 * @param nom
+	 * @param prenom
+	 * @param adresse
+	 */
 	public Personne(String nom, String prenom, AdressePostale adresse) {
-		
+
 		this.nom = nom;
 		this.prenom = prenom;
 		this.adresse = adresse;
-}}
+	}
+
+	// TP 3
+	/*
+	 * Q1 : Dans la classe Personne, ajoutez une méthode qui permet d’afficher le
+	 * nom et le prénom avec le nom de famille en majuscules.
+	 */
+
+	public void afficheridentite() {
+		System.out.println("prenom" + " " + nom.toUpperCase());
+	}
+	/*
+	 * Q2Dans la classe Personne, ajoutez une méthode qui prend un argument en
+	 * paramètre et permet de modifier la variable d’instance nom de Personne.
+	 */
+
+	public void SetNom(String nvNom) { // Set = setter permettent de modif les attributs d'instance)
+		nom = nvNom;
+	}
+	/*
+	 * Q3 : Dans la classe Personne, ajoutez une méthode similaire pour modifier le
+	 * prénom.
+	 */
+
+	public void SetPrenom(String nvPrenom) {
+		prenom = nvPrenom;
+	}
+
+	/*
+	 * Q4 : Dans la classe Personne, ajoutez une méthode similaire pour modifier
+	 * l’adresse.
+	 */
+
+	public void SetAdresse(AdressePostale nvAd) {
+		adresse = nvAd;
+	}
+	/* Q5 : Dans la classe Personne, ajoutez une méthode qui retourne le nom. */
+
+	public String getNom() {
+		return nom;
+	}
+	/* Q6 : Dans la classe Personne, ajoutez une méthode qui retourne le prénom. */
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/* Q7 : Dans la classe Personne, ajoutez une méthode qui retourne l'adresse. */
+
+	public AdressePostale getAdresse() {
+		return adresse;
+	}
+
+}
--- src/essai/TestPersonne.java
+++ src/essai/TestPersonne.java
@@ -12,6 +12,21 @@ public class TestPersonne {
 		AdressePostale a2 = new AdressePostale(2, "du chien", 44000, "Nantes");
 		Personne p2 = new Personne("Dup", "Paul", a2);
 
+		/*
+		 * TP3.Q7 : Modifier la classe TestPersonne de manière à o utiliser le
+		 * constructeur avec arguments de la classe Personne pour valoriser les
+		 * attributs nom et prenom o utiliser la méthode qui permet de modifier
+		 * l’adresse pour valoriser l’adresse.
+		 */
+		
+		String Nom1 = p1.getNom();
+		System.out.println(Nom1);
+
+		String Prenom1 = p1.getPrenom();
+		System.out.println(Prenom1);
+		
+		AdressePostale Adr1 = p1.getAdresse();
+		System.out.println(Adr1);
 	}
 
 }
