36dfb1d36ff888cbd41d38ca38deabd3f7685570
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-03 12:13:00 +0100

    TP1

---
+++ src/entites/AdressePostale.java
@@ -0,0 +1,8 @@
+package entites;
+
+public class AdressePostale {
+	public int numRue;
+	public String rue;
+	public int codePostal;
+	public String ville;
+}
---
+++ src/essai/TestPersonne.java
@@ -0,0 +1,15 @@
+package essai;
+
+import entites.Personne;
+
+public class TestPersonne {
+
+	public static void main(String[] args) {
+		Personne p = new Personne();
+p.nom="Sagan";
+p.prenom="jonathan";
+p.adresse=a;
+
+	}
+
+}
