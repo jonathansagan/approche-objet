8cb86dfceb3b1a5206c0f56873d4c0a481b24d21
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-04 14:05:25 +0100

    TP2 Redéfinition de méthode

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -4,9 +4,13 @@ import fr.diginamic.banque.entites.Compte;
 
 public class TestBanque {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Compte a = new Compte("F1", 2000.0);
 		System.out.println(a);
+		
 	} 
 	
 
--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -10,4 +10,28 @@ public class Compte {
 		this.solde = solde;
 	}
 
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	/**
+	 *
+	 */
+	@Override
+	
+	public String toString() {// ici string to string permettra de visualiser correcement le contenu de compte
+		return "Compte numéro : " + num + ", solde = " + solde;
+	}
+
 }
