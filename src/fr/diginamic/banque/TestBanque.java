37db1a950f0d82b484763aa895b6895a7a7cf4ea
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-04 11:16:27 +0100

    TP1 encapsulation ok

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -5,7 +5,7 @@ import fr.diginamic.banque.entites.Compte;
 public class TestBanque {
 
 	public static void main(String[] args) {
-		Compte a = new Compte(1, 2000.0);
+		Compte a = new Compte("F1", 2000.0);
 		System.out.println(a);
 	} 
 	
--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -1,12 +1,12 @@
 package fr.diginamic.banque.entites;
 
 public class Compte {
-	public int numCompte; // attributs
-	public double solde; //attributs
+	private String num; // attributs
+	private double solde; //attributs
 	
-	public Compte(int numCompte, double solde) {//constructeur
+	public Compte(String num, double solde) {//constructeur
 		
-		this.numCompte = numCompte;
+		this.num = num;
 		this.solde = solde;
 	}
 
