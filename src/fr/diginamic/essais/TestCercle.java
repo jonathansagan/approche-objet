6337f120b725165cc0befd16d8f49eabb6e9acdb
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-06 10:28:01 +0100

    TP3 Exercice 2 : CercleFactory

--- src/fr/diginamic/essais/TestCercle.java
+++ src/fr/diginamic/essais/TestCercle.java
@@ -1,6 +1,7 @@
 package fr.diginamic.essais;
 
 import fr.diginamic.entites.Cercle;
+import fr.diginamic.utils.CercleFactory;
 
 public class TestCercle {
 
@@ -13,6 +14,17 @@ public class TestCercle {
 		Cercle cercle2 = new Cercle(2.5);
 		System.out.println("Le périmètre du cercle 2 est : " + cercle2.perimetre());
 		System.out.println("La surface du cercle 2 est : " + cercle2.surface());
+		
+		Cercle cercle3 = CercleFactory.creerCercle (4.0); 
+		System.out.println("Le périmètre du cercle 3 est : " + cercle3.perimetre());
+		System.out.println("La surface du cercle 3 est : " + cercle3.surface());
 	}
 
 }
+/**méthode pour retourner un cercle avec en paramètre un nouveau rayon
+* @param nrayon = nouveau paramètre de rayon
+* @return cercle
+
+public static Cercle creerCercle(double nrayon) {
+	Cercle cer = new Cercle (nrayon);
+	return cer;*/
