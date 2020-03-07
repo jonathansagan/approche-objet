7335ea43a83803620dae35aa0dfb3cab59570ff5
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-07 18:08:13 +0100

    TP3 Exercice 7 Salaire ok

--- src/fr/diginamic/salaire/TestIntervenant.java
+++ src/fr/diginamic/salaire/TestIntervenant.java
@@ -6,9 +6,9 @@ public class TestIntervenant {
 	public static void main(String[] args) {
 		
 		Salarie s1 = new Salarie ("Harry", "Potter", 1950);
-		System.out.println("Le salaire du salarié "+ s1+ "est de : "+s1.getSalaire()+"€");
+		System.out.println("Le salaire du salarié est de : "+s1.getSalaire()+" €");
 		Pigiste p1 = new Pigiste("Drago", "Malfoy", 40.2,4);
-		System.out.println("Le salaire du pigiste "+ p1+ "est de : "+p1.getSalaire()+"€");
+		System.out.println("Le salaire du pigiste est de : "+p1.getSalaire()+" €");
 
 	}
 
