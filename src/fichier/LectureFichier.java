73034848f9a79cb7019cee9b6a19870ebefbc872
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-10 11:14:35 +0100

    TP guidé : Lecture d'un fichier

---
+++ src/fichier/LectureFichier.java
@@ -0,0 +1,21 @@
+package fichier;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+public class LectureFichier {
+
+	public static void main(String[] args) throws IOException {
+		File file = new File("C:/temp/recensement population 2016.csv");
+		List<String> lignes = FileUtils.readLines(file, "UTF-8");
+		for (int i = 1; i < lignes.size(); i++)
+			
+		{
+			System.out.println(lignes.get(i));
+		}
+
+	}
+}
