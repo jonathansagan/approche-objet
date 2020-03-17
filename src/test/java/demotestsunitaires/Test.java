b456ff14d5a751afc9325569cc4161cae63ef353
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-03-17 12:13:00 +0100

    TP : Les Tests unitaires

---
+++ src/test/java/demotestsunitaires/Test.java
@@ -0,0 +1,12 @@
+package demotestsunitaires;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void testLevenshteinDistance() {
+		fail("Not yet implemented");
+	}
+
+}
